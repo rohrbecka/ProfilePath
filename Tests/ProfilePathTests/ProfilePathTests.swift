deb15f04a2b174c774a98ea86484e1f657cb3cc6
André Rohrbeck <andre.rohrbeck@tuvsud.com>
2022-12-30 11:04:45 +0100

    Initial Commit

---
+++ Tests/ProfilePathTests/ProfilePathTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ProfilePath
+
+final class ProfilePathTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ProfilePath().text, "Hello, World!")
+    }
+}
